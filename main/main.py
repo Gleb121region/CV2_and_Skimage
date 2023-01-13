ef27818a1e55d7fee221a01e69a80a3533a7b2a8
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-01-13 03:03:45 +0300

    add bit

--- main/main.py
+++ main/main.py
@@ -1,50 +1,63 @@
-import fnmatch
 import os
+import pathlib
 
 import cv2
+import numpy as np
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 
 
-def predict(file_path: str, name_dir: str):
-    for image_name in give_name_file(name_dir):
-        path = name_dir + image_name
+def give_list_frame_path(name_dir: str) -> [str]:
+    res = []
+    d = pathlib.Path(name_dir)
+    for entry in d.iterdir():
+        if entry.is_file():
+            res.append(str(entry))
+    return res
+
+
+def mse(imageA, imageB):
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    return err
+
+
+def predict(file_path: str, folder_path: str):
+    tmp_simular_cof = -1
+    tmp_mse = float(10000)
+
+    for image_name in give_list_frame_path(folder_path):
         img = cv2.imread(file_path)
-        try:
-            ref = cv2.imread(path)
-            gray1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray2 = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
-            (simular, diff) = ssim(gray1, gray2, full=True)
-            if simular > 0.5:
-                print(simular)
-                print(path)
-        except:
-            print('похуй')
+        ref = cv2.imread(image_name)
+        gray1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.cvtColor(ref, cv2.COLOR_BGR2GRAY)
+        (simular, diff) = ssim(gray1, gray2, full=True)
+        mse_cof = mse(img, ref)
+        if simular > tmp_simular_cof:
+            tmp_simular_cof = simular
+            print(image_name)
+
+        if mse_cof < tmp_mse:
+            tmp_mse = mse_cof
+            print(mse_cof)
+            print(image_name)
+    print(f'mse: {tmp_mse} \n simular: {tmp_simular_cof}')
 
 
-def give_name_file(name_dir: str) -> [str]:
-    listOfFiles = os.listdir(name_dir)
-    pattern = "*.jpg"
-    list_name_photo = []
-    for entry in listOfFiles:
-        if fnmatch.fnmatch(entry, pattern):
-            list_name_photo.append(entry)
-    return listOfFiles
-
-
-def extractImages(pathIn: str, pathOut: str):
+def extract_images(path_in: str, path_out: str):
     count = 0
-    vidcap = cv2.VideoCapture(pathIn)
-    success, image = vidcap.read()
+    video_capture = cv2.VideoCapture(path_in)
+    success, image = video_capture.read()
     success = True
     while success:
         try:
-            vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
-            success, image = vidcap.read()
-            cv2.imwrite(pathOut + "frame%d.jpg" % count, image)
+            video_capture.set(cv2.CAP_PROP_POS_MSEC, (count * 1000))
+            success, image = video_capture.read()
+            photo_path = path_out + "frame%d.jpg" % count
+            cv2.imwrite(photo_path, image)
             count = count + 1
         except:
-            print('все')
+            print('The video is over')
 
 
 def get_size_format(b, factor=1024, suffix="B"):
@@ -83,27 +96,39 @@ def compress_img(image_name, new_size_ratio, quality, width=None, height=None, t
     print(f"[+] Image size change: {saving_diff / image_size * 100:.2f}% of the original image size.")
 
 
-if __name__ == "__main__":
-    name_dir: str = 'frams/'
-    name_dir_frame: str = '/Users/popovgleb/PycharmProjects/pythonProject4/myFrams/'
+def compress_files_from_dir(dir_name: str) -> None:
+    for image_name in give_list_frame_path(dir_name):
+        try:
+            compress_img(image_name=image_name, new_size_ratio=1, quality=75, width=256, height=256, to_jpg=True)
+        except:
+            pass
 
-    predict('/Users/popovgleb/PycharmProjects/pythonProject4/myFrams/Снимок экрана 2023-01-11 в 14.01.58.jpg',
-            name_dir)
 
-# extractImages(pathIn='/Users/popovgleb/PycharmProjects/pythonProject4/video/Друзья_S10_E2_1080.mp4',
-#               pathOut=name_dir)
-# for image_name in give_name_file(name_dir):
-#     path = name_dir + image_name
-#     print(path)
-#     try:
-#         compress_img(image_name=path, new_size_ratio=0.9, quality=75, width=2880, height=1800, to_jpg=True)
-#     except:
-#         print("похуй")
+def create_dir_by_parent_dir_and_video_name_and_return_path_name(video_path: str, parent_dir: str) -> str:
+    directory = video_path.replace('/Users/popovgleb/PycharmProjects/pythonProject4/video/', '').replace('.mp4', '')
+    path = os.path.join(parent_dir, directory)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    return path
 
-# for image_name in give_name_file(name_dir_frame):
-#     path = name_dir_frame + image_name
-#     print(path)
-#     try:
-#         compress_img(image_name=path, new_size_ratio=200, quality=75, width=1152, height=648, to_jpg=True)
-#     except:
-#         print('похуй')
+
+def gg(video_path: str, parent_dir: str):
+    path: str = f'{create_dir_by_parent_dir_and_video_name_and_return_path_name(video_path, parent_dir)}'
+    extract_images(video_path, path)
+    compress_files_from_dir(path)
+
+# def ffmpeg(video_path:str , root_dir_path:str):
+#     "ffmpeg -i Шрек_1080.mp4 -r 1 -vf scale=256:256  image-%05d.jpeg"
+
+def main():
+    dir_path: str = '/Users/popovgleb/PycharmProjects/pythonProject4/frams/Шрек_1080/'
+    dir_frame_path: str = '/Users/popovgleb/PycharmProjects/pythonProject4/myFrams/'
+
+    photo_path: str = '/Users/popovgleb/PycharmProjects/pythonProject4/myFrams/Снимок экрана 2023-01-13 в 02.41.02.jpg'
+
+    compress_files_from_dir(dir_frame_path)
+    predict(photo_path, dir_path)
+
+
+if __name__ == '__main__':
+    main()
